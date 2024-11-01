0720b56569ea455801818e9ec1f85a74c74c6cb6
dineshkanna <dineshkanna654@gmail.com>
2024-11-01 16:43:40 +0530

    Date integrated

--- Components/DateFormat.py
+++ Components/DateFormat.py
@@ -1,10 +1,4 @@
 from datetime import datetime
-import pandas as pd
-
-file_path = 'Diff format.csv'
-df = pd.read_csv(file_path)
-
-print(df.columns)
 
 
 def standardize_date(date):
@@ -23,25 +17,7 @@ def standardize_date(date):
             continue 
     return date  
 
-df['Date'] = df['Date'].apply(standardize_date)
 
-df.to_csv('cleaned_file.csv', index=False)
-
-
-df = pd.read_csv(file_path)
-
-ori = ['Date', 'Customer Number', 'Account Number', 'Account Name',
-       'Account Class', 'Account Status', 'Account Currency',
-       'Alternative Currency', 'Outstanding Amount', 'Up 30days', 'Up 60days',
-       'Up 90days', 'Up 120days', 'Up 150days', 'Up 180days', 'Up 210days',   
-       'Up 240days', 'Up 270days', 'Up 300days', 'Up 330days', 'Up 364days',  
-       'Up 365days+']
-
-err = ['Date', 'Account Name', 'Acc Num', 'Account Class', 'Account Status',  
-       'Cust Num', 'Account Currency', 'Alternative Currency',
-       'Outstanding Amt', 'Up 30days', 'Up 60days', 'Up 90days', 'Up 120days',
-       'Up 150days', 'Up 180days', 'Up 210days', 'Up 240days', 'Up 270days',  
-       'Up 300days', 'Up 330days', 'Up 364days', 'Up 365days+']
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from Components.order_spell import correct_mistaken_file
+from Components.DateFormat import standardize_date
+import pandas as pd
 
 # Now you can use the function
 correct_mistaken_file('SOA_Reff_num.csv', 'Diff format.csv', 'corrected_mistaken_file.csv')
+
+file_path = 'corrected_mistaken_file.csv'
+df = pd.read_csv(file_path)
+
+df['Date'] = df['Date'].apply(standardize_date)
+
+df.to_csv('corrected_mistaken_file.csv', index=False)
+
+print(df.columns)
+
 
