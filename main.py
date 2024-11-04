7a8982471b3b382681ce89ba4fe28782cefe5380
dineshkanna <dineshkanna654@gmail.com>
2024-11-04 16:47:31 +0530

    rm

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import pandas as pd
 # arrange the correct order and spell correct 
 correct_mistaken_file('SOA_Reff_num.csv', 'Diff format.csv', 'corrected_mistaken_file.csv')
 
-
 # Fix the Date in only one format
 file_path = 'corrected_mistaken_file.csv'
 df = pd.read_csv(file_path)
